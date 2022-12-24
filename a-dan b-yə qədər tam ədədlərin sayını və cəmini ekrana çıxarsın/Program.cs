e5700715931b36171fd2ffb3b90a9aeb2ecd90a3
huseynManaf <huseynim@code.edu.az>
2022-12-24 05:36:56 -0800

    init

---
+++ a-dan b-yə qədər tam ədədlərin sayını və cəmini ekrana çıxarsın/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace a_dan_b_yə_qədər_tam_ədədlərin_sayını_və_cəmini_ekrana_çıxarsın
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {             
+            Console.Write("Baslangic ededi daxil edin: ");
+                      int a = int.Parse(Console.ReadLine());
+                      Console.Write("Son ededi daxil edin: ");
+                      int b = int.Parse(Console.ReadLine());
+
+            int count = 0;
+             int sum = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                sum += i;
+                count++;
+            }
+
+            Console.WriteLine($"\nEdedlerin sayi: {count}\nEdedlerin cemi: {sum}");
+        }
+    }
+}
+       
